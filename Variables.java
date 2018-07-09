0c4370eab7d794429b02467432bc4177a3a95bea
Owen1128 <40574548+Owen1128@users.noreply.github.com>
2018-07-09 10:30:13 -0700

    Lesson One

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+	public static void main(String[] args) {
+		String name = "Jonavon";
+		int age = 19;
+		double mpg = 6;
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(mpg);
+	}
+}
---
+++ Variables2.java
@@ -0,0 +1,7 @@
+public class Variables2 {
+	public static void main(String[] args) {
+		String firstName = "Jong Un";
+		String lastName = "Kim ";
+		System.out.println(lastName + firstName);
+	}
+}
